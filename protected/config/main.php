1dd70cd6f61a6f0a980232c34a448389029311ea
melmiranda <melania.smc@hotmail.com>
2017-01-30 11:55:43 -0300

    11:55 30-01-2016

--- protected/config/main.php
+++ protected/config/main.php
@@ -45,6 +45,11 @@ return array(
 			'rules'=>array(
 
 
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+
+
 				"<data>"=>array(
 					"web/get/"
 				),
