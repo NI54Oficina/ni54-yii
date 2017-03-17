a62ca0d2e4a9fa3dd97550e86af30859cc14fa89
melmiranda <melania.smc@hotmail.com>
2017-03-17 17:01:09 -0300

    :17:00 17-03-2017

---
+++ protected/views/static/admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ADMINISTRADOR</title>
+  </head>
+  <body>
+
+      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/img/nube-03.svg">
+        <div class="">
+          <p>INGRESAR PROYECTOS</p>
+        </div>
+      </a>
+
+      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/img/nube-03.svg">
+        <div class="">
+          <p>INGRESAR PROYECTOS</p>
+        </div>
+      </a>
+
+
+  </body>
+</html>
