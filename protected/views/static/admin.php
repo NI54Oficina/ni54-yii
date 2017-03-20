b75ea7039e6fa5170b9cc8f91c9d1f670ad287b3
melmiranda <melania.smc@hotmail.com>
2017-03-20 11:32:34 -0300

    11:32 20-03-2017

--- protected/views/static/admin.php
+++ protected/views/static/admin.php
@@ -4,17 +4,68 @@
     <meta charset="utf-8">
     <title>ADMINISTRADOR</title>
   </head>
+
+  <style media="screen">
+
+    body{
+      padding:100px;
+      background-color: #dedcdc;
+    }
+    .button{
+      padding: 20px;
+      display:inline-flex;
+
+    }
+
+    .button p{
+      color: white;
+      background: linear-gradient( #39b4e0, #2ea2cb);
+      padding: 30px;
+      border: 1px solid #39b4e0;
+      border-radius: 5px;
+      font-family: verdana;
+    }
+
+    .button p:hover{
+      opacity: .5;
+    }
+
+    a{
+      outline: none;
+      text-transform: none;
+      text-decoration: none;
+    }
+
+  </style>
   <body>
 
-      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/img/nube-03.svg">
-        <div class="">
+      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/index.php/project/create" target="_blank">
+        <div class="button">
           <p>INGRESAR PROYECTOS</p>
         </div>
       </a>
 
-      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/img/nube-03.svg">
-        <div class="">
-          <p>INGRESAR PROYECTOS</p>
+      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/index.php/pantalla/create" target="_blank">
+        <div class="button">
+          <p>INGRESAR SOLAPAS POR PROYECTOS</p>
+        </div>
+      </a>
+
+      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/index.php/imgPantalla/create" target="_blank">
+        <div class="button">
+          <p>INGRESAR IMAGENES POR SOLAPAS</p>
+        </div>
+      </a>
+
+      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/index.php/clientes/create" target="_blank">
+        <div class="button">
+          <p>INGRESAR CLIENTES</p>
+        </div>
+      </a>
+
+      <a href="<?php echo Yii::app()->getBaseUrl(true); ?>/index.php/partners/create" target="_blank">
+        <div class="button">
+          <p>INGRESAR PARTNERS</p>
         </div>
       </a>
 
